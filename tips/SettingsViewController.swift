19e26e14d8a1fd971310885663deeed30c72cfe3
Erin Walsh <erinwalsh@erins-mbp.lfytina2.metronetinc.com>
2015-12-09 10:01:26 -0500

    initial commit

---
+++ tips/SettingsViewController.swift
@@ -0,0 +1,49 @@
+//
+//  SettingsViewController.swift
+//  tips
+//
+//  Created by Erin Walsh on 12/8/15.
+//  Copyright © 2015 Jeremy Lehman. All rights reserved.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+
+    @IBOutlet weak var tipControl: UISegmentedControl!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        let defaults = NSUserDefaults.standardUserDefaults()
+        if(defaults.objectForKey("tipIndex") != nil){
+            tipControl.selectedSegmentIndex = defaults.integerForKey("tipIndex")
+        }
+            
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    override func viewWillDisappear(animated: Bool) {
+        super.viewWillDisappear(animated)
+        let defaults = NSUserDefaults.standardUserDefaults()
+        defaults.setInteger(tipControl.selectedSegmentIndex,forKey: "tipIndex")
+        defaults.synchronize()
+        
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- tips/ViewController.swift
+++ tips/ViewController.swift
@@ -2,7 +2,7 @@
 //  ViewController.swift
 //  tips
 //
-//  Created by Erin Walsh on 12/7/15.
+//  Created by Jeremy Lehman on 12/7/15.
 //  Copyright © 2015 Jeremy Lehman. All rights reserved.
 //
 
@@ -10,9 +10,34 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tipControl: UISegmentedControl!
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var billField: UITextField!
+    @IBOutlet weak var tipLabel: UILabel!
+    var tipSettings = 0
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        tipLabel.text = "$0.00"
+        totalLabel.text = "$0.00"
+        
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        var percentages = [0.18,0.22,0.25]
+        let defaults = NSUserDefaults.standardUserDefaults()
+        if(defaults.objectForKey("tipIndex") != nil){
+            let index = defaults.integerForKey("tipIndex")
+            tipControl.selectedSegmentIndex = index
+        
+            let percentage = percentages[tipControl.selectedSegmentIndex]
+            let billAmount = NSString(string:billField.text!).doubleValue
+            let tip = billAmount * percentage
+            let total = billAmount + tip
+            tipLabel.text = String(format:"$%.2f",tip)
+            totalLabel.text = String(format:"$%.2f",total)
+        }
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +45,20 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func onEditingChanged(sender: AnyObject) {
+        var tipPercentages = [0.18,0.22,0.25]
+        let percentage = tipPercentages[tipControl.selectedSegmentIndex]
+        let billAmount = NSString(string: billField.text!).doubleValue
+        let tip = billAmount * percentage
+        let total = billAmount + tip
+        tipLabel.text = String(format:"$%.2f",tip)
+        totalLabel.text = String(format:"$%.2f",total)
+    }
+    
+    @IBAction func onTap(sender: AnyObject) {
+        view.endEditing(true)
+    }
 
+ 
 }
 
